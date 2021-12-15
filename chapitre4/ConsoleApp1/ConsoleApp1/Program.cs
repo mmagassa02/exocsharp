99e54b903cada7898b2f7f1d29ca7a912cc1ffe8
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-15 16:18:16 +0100

    Mise a jour exo chp 3 et 4

--- chapitre4/ConsoleApp1/ConsoleApp1/Program.cs
+++ chapitre4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Création d'un tableau
             string[] week = new string[7];
             week[0] = "Lundi";
             week[1] = "Mardi";
@@ -14,13 +15,16 @@ namespace ConsoleApp1
             week[4] = "Vendredi";
             week[5] = "Samedi";
             week[6] = "Dimanche";
+
+            //Affichage de valeurs
             Console.WriteLine(week[1]+ "\n"+week[4]);
             Console.WriteLine(week[0] + "\n" + week[3]);
-            //Tableau corrigé
+            //Modification de "Jedi" par "Jeudi"
             week[3] = "Jeudi";
             Console.WriteLine("\n");
             Console.WriteLine(week[3]);
             Console.WriteLine("\n");
+            //Affichage du tableau corrigé
             for (int i = 0; i < 7; i++)
                 Console.WriteLine(week[i]);
 
--- chapitre4/ConsoleApp2/ConsoleApp2/Program.cs
+++ chapitre4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,17 +7,27 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            // Liste de diiferent langages
             List <string> languages = new List<string> {"HTML", "CSS", "Javacrit", "jQuery", "PHP", "Bootstrap", "Java" };
+
+            //On affiche la totalité de la liste grâce à une boucle
             for (int i = 0; i < languages.Count; i++) 
             {
                 Console.WriteLine(languages[i]);
              }
+
             Console.WriteLine();
+
             Console.WriteLine(languages[3]);
             Console.WriteLine(languages[4]);
+            
+            //Suppression d'une valeur dans la liste
             languages.Remove("Bootstrap");
+            //Modification d'une valeur dans la liste
             languages[2] = "Javascript";
+            //Ajout d'une valeur supplémentaire dans la liste.  
             languages.Add("C");
+            // On réaffiche la liste modifiée
             Console.WriteLine();
             for (int i = 0; i < languages.Count; i++)
             {
