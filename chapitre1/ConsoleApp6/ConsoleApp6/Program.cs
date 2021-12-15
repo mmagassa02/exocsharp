3e9cd46e62f2e659e1dbc96eddc6d13fc3fc7b23
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-15 11:56:42 +0100

    Ajout exo 6

---
+++ chapitre1/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom, prenom;
+            int annee;
+            Console.WriteLine("Saisir votre nom");
+            nom = Console.ReadLine();
+            Console.WriteLine("Saisir votre prénom");
+            prenom = Console.ReadLine();
+            Console.WriteLine("Saisir votre année de naissance");
+            annee = int.Parse(Console.ReadLine());
+            Console.WriteLine("Fiche de renseignement\n\nNom: " + nom + "\n\nPrénom: " + prenom + "\n\nAge: " + (2021 - annee));
+        }
+    }
+}
