0bcb8da1e8908eaa517d6d56892c5fee7613195a
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-15 12:32:11 +0100

    Ajout ex5 chp2

---
+++ chapitre2/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mois;
+            Console.WriteLine("Entrer un mois");
+            mois = Console.ReadLine();
+            if (mois == "decembre" ^ mois == "janvier" ^ mois == "fevrier")
+                Console.WriteLine("La saison du mois saisi est l'HIVER.");
+            else if (mois == "mars" ^ mois == "avril" ^ mois == "mai")
+                Console.WriteLine("La saison du mois saisi est le PRINTEMPS.");
+            else if (mois == "juin" ^ mois == "juillet" ^ mois == "aout")
+                Console.WriteLine("La saison du mois saisi est l'ÉTÉ.");
+            else if (mois == "septembre" ^ mois == "octobre" ^ mois == "novembre")
+                Console.WriteLine("La saison du mois saisi est l'AUTOMNE.");
+            else
+                Console.WriteLine("Mauvaise saisie");
+
+        }
+    }
+}
