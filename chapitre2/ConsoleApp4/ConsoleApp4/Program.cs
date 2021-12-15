cf2adefbe507c3cf2ae9627bfb3a885856ea88b0
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-15 12:19:56 +0100

    Ajout ex4 chp2

---
+++ chapitre2/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, password;
+            Console.WriteLine("Saisir identifiant");
+            login = Console.ReadLine();
+            Console.WriteLine("Saisir mot de passe");
+            password = Console.ReadLine();
+            switch (login)
+            {
+                case "mmagassa":
+                    if (password == "123456")
+                        Console.WriteLine("Bienvenue à la manu");
+                    
+                    else
+                        Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+                    break;
+                default:
+                    Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+                    break;
+            }
+        }
+    }
+}
