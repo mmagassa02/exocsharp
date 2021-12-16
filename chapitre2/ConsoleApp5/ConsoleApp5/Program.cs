434fc9969f2ff163466b4f0079de00608527ab30
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-16 10:16:59 +0100

    MAJ correction exercice 5 chp 2(variables en anglais)

--- chapitre2/ConsoleApp5/ConsoleApp5/Program.cs
+++ chapitre2/ConsoleApp5/ConsoleApp5/Program.cs
@@ -6,17 +6,17 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            string mois;
-            Console.WriteLine("Entrer un mois");
-            mois = Console.ReadLine();
-            if (mois == "decembre" ^ mois == "janvier" ^ mois == "fevrier")
-                Console.WriteLine("La saison du mois saisi est l'HIVER.");
-            else if (mois == "mars" ^ mois == "avril" ^ mois == "mai")
-                Console.WriteLine("La saison du mois saisi est le PRINTEMPS.");
-            else if (mois == "juin" ^ mois == "juillet" ^ mois == "aout")
-                Console.WriteLine("La saison du mois saisi est l'ÉTÉ.");
-            else if (mois == "septembre" ^ mois == "octobre" ^ mois == "novembre")
-                Console.WriteLine("La saison du mois saisi est l'AUTOMNE.");
+            string month;
+            Console.WriteLine("Entrer un month");
+            month = Console.ReadLine();
+            if (month == "decembre" ^ month == "janvier" ^ month == "fevrier")
+                Console.WriteLine("La saison du month saisi est l'HIVER.");
+            else if (month == "mars" ^ month == "avril" ^ month == "mai")
+                Console.WriteLine("La saison du month saisi est le PRINTEMPS.");
+            else if (month == "juin" ^ month == "juillet" ^ month == "aout")
+                Console.WriteLine("La saison du month saisi est l'ÉTÉ.");
+            else if (month == "septembre" ^ month == "octobre" ^ month == "novembre")
+                Console.WriteLine("La saison du month saisi est l'AUTOMNE.");
             else
                 Console.WriteLine("Mauvaise saisie");
 
