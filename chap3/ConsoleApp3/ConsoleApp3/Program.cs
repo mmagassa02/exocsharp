fd40290d167d0b0b9e24b7431c52f60d9d4a64f0
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-16 12:34:53 +0100

    Maj exercice 4 chap3

--- chap3/ConsoleApp3/ConsoleApp3/Program.cs
+++ chap3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -13,11 +13,15 @@ namespace ConsoleApp3
             {
                 Console.WriteLine("Entrer un nombre a additionner");
                 result = result + int.Parse(Console.ReadLine());
+                //Pour revenir à ce point si l'utilisateur ne saisit pas oui ou non à la question suivante.
+                question:
                 Console.WriteLine("Voulez vous ajouter un nombre? (Taper oui ou non)");
                 saisie = Console.ReadLine();
                 //L'utilisateur tape non pour ne pas ajouter de nombres à l'addition.
                 if (saisie == "non")
                     break;
+                else if (saisie != "oui")
+                    goto question;  //On retourne à la ligne 17 plus haut pour reposer la question.
 
             }
             Console.WriteLine(result);
